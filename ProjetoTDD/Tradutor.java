8b39b78389b01f791a861a3a6fb1188b94da7c7a
Wiliam Trancoso <wileleh@gmail.com>
2023-06-05 20:39:17 -0300

    Primeira aula

--- ProjetoTDD/Tradutor.java
+++ ProjetoTDD/Tradutor.java
@@ -1,1 +1,22 @@
+package sample;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Tradutor {
+    
+    private HashMap<String, String> traducoes = new HashMap<String, String>() {};
+
+    public String traduzir(String palavra) {
+        return traducoes.get(palavra);
+    }
+    
+    public void adicionarTraducao(String palavra, String traducao) {
+        traducoes.put(palavra, traducao);
+    }
+
+    public boolean estaVazio() {
+        return traducoes.isEmpty();
+    }
+    
+}
--- ProjetoTDD/TradutorTest.java
+++ ProjetoTDD/TradutorTest.java
@@ -1,1 +1,59 @@
+package sample;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TradutorTest {
+
+    @Test
+    public void When_estaVazio_WithoutWord_Expected_True() {
+        Tradutor tradutor = new Tradutor();
+        boolean actual = tradutor.estaVazio();
+
+        assertTrue(actual);
+    }
+
+    @Test
+    public void When_estaVazio_WithWord_Expected_False() {
+        Tradutor tradutor = new Tradutor();
+        tradutor.adicionarTraducao("bom", "good");
+        boolean actual = tradutor.estaVazio();
+
+        assertFalse(actual);
+    }
+
+    @Test
+    public void When_traduzir_bom_Expected_Good() {
+        Tradutor tradutor = new Tradutor();
+        String expected = "good";
+        
+        tradutor.adicionarTraducao("bom", "good");
+        String actual = tradutor.traduzir("bom");
+
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void When_AddTwoWord_And_traduzir_First_Expected_First() {
+        Tradutor tradutor = new Tradutor();
+        String expected = "good";
+        
+        tradutor.adicionarTraducao("bom", "good");
+        tradutor.adicionarTraducao("mau", "bad");
+        String actual = tradutor.traduzir("bom");
+
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void When_AddTwoWord_And_traduzir_Second_Expected_Second() {
+        Tradutor tradutor = new Tradutor();
+        String expected = "bad";
+        
+        tradutor.adicionarTraducao("bom", "good");
+        tradutor.adicionarTraducao("mau", "bad");
+        String actual = tradutor.traduzir("mau");
+
+        assertEquals(expected, actual);
+    }
+}
